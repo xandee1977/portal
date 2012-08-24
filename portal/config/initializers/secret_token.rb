a60ea5c0eccb2a7a94fb279cc935fb64f0b9912e
tvjurere <tvjurere@pegasus.dreamhost.com>
2012-08-23 17:19:40 -0700

    site v1

---
+++ portal/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Portal::Application.config.secret_token = '65b731b39ad93db318f52ddf7ba546a4b39f2742ff441a405f040584b5bd492446302b3e3a4da58a108c6b7e3e3560fdd84e74ad538f8279ad3e27a2f8a83927'
